1a53d136b388e53e8b6a8e9d8fb3dccb39828230
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-24 15:32:19 +0100

    Add billing frequency toggle

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,20 @@ import React from "react";
 
 import SEO from "../components/seo";
 
-const IndexPage = () => <SEO title="Home" />;
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <h1>Our Pricing</h1>
+    <input
+      type="radio"
+      name="billing-frequency"
+      id="annually"
+      value="annually"
+    />
+    <label htmlFor="annually">Annually</label>
+    <input type="radio" name="billing-frequency" id="monthly" value="monthly" />
+    <label htmlFor="monthly">Monthly</label>
+  </>
+);
 
 export default IndexPage;
