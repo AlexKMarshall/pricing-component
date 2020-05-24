c926fc46a35082b145155584f2dec3f39b30a579
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-24 16:18:05 +0100

    Control frequency selector from state

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <>
-    <SEO title="Home" />
-    <h1>Our Pricing</h1>
-    <input
-      type="radio"
-      name="billing-frequency"
-      id="annually"
-      value="annually"
-    />
-    <label htmlFor="annually">Annually</label>
-    <input type="radio" name="billing-frequency" id="monthly" value="monthly" />
-    <label htmlFor="monthly">Monthly</label>
-    <PricingTier
-      tierName="Basic"
-      monthlyRate={getPrice("basic", "monthly")}
-      storage="500 GB"
-      users="2"
-      transferLimit="3 GB"
-    />
-    <PricingTier
-      tierName="Professional"
-      monthlyRate={getPrice("professional", "monthly")}
-      storage="1 TB"
-      users="5"
-      transferLimit="10 GB"
-    />
-    <PricingTier
-      tierName="Master"
-      monthlyRate={getPrice("master", "monthly")}
-      storage="2 TB"
-      users="10"
-      transferLimit="20 GB"
-    />
-  </>
-);
+const IndexPage = () => {
+  const [billingFrequency, setBillingFrequency] = useState("monthly");
 
-function PricingTier({ tierName, monthlyRate, storage, users, transferLimit }) {
+  const handleOptionChange = (event) => {
+    setBillingFrequency(event.target.value);
+  };
+
   return (
     <>
-      <h2>{tierName}</h2>
-      <div>${monthlyRate}</div>
-      <ul>
-        <li>{storage} Storage</li>
-        <li>{users} Users Allowed</li>
-        <li>Send up to {transferLimit}</li>
-      </ul>
-      <button>Learn more</button>
+      <SEO title="Home" />
+      <h1>Our Pricing</h1>
+      <input
+        type="radio"
+        name="billing-frequency"
+        id="annually"
+        checked={billingFrequency === "annually"}
+        onChange={handleOptionChange}
+        value="annually"
+      />
+      <label htmlFor="annually">Annually</label>
+      <input
+        type="radio"
+        name="billing-frequency"
+        id="monthly"
+        checked={billingFrequency === "monthly"}
+        onChange={handleOptionChange}
+        value="monthly"
+      />
+      <label htmlFor="monthly">Monthly</label>
+      <PricingTier
+        tierName="Basic"
+        monthlyRate={getPrice("basic", "monthly")}
+        storage="500 GB"
+        users="2"
+        transferLimit="3 GB"
+      />
+      <PricingTier
+        tierName="Professional"
+        monthlyRate={getPrice("professional", "monthly")}
+        storage="1 TB"
+        users="5"
+        transferLimit="10 GB"
+      />
+      <PricingTier
+        tierName="Master"
+        monthlyRate={getPrice("master", "monthly")}
+        storage="2 TB"
+        users="10"
+        transferLimit="20 GB"
+      />
     </>
   );
-}
+
+  function PricingTier({
+    tierName,
+    monthlyRate,
+    storage,
+    users,
+    transferLimit,
+  }) {
+    return (
+      <>
+        <h2>{tierName}</h2>
+        <div>${monthlyRate}</div>
+        <ul>
+          <li>{storage} Storage</li>
+          <li>{users} Users Allowed</li>
+          <li>Send up to {transferLimit}</li>
+        </ul>
+        <button>Learn more</button>
+      </>
+    );
+  }
+};
 
 function getPrice(pricingTier, billingFrequency) {
   const priceList = {
