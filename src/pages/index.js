ef474cb404cec5e2cb6b689efe35918d2f8f76aa
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-24 15:38:24 +0100

    Add markup for first pricing option

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,14 @@ const IndexPage = () => (
     <label htmlFor="annually">Annually</label>
     <input type="radio" name="billing-frequency" id="monthly" value="monthly" />
     <label htmlFor="monthly">Monthly</label>
+    <h2>Basic</h2>
+    <div>$19.99</div>
+    <ul>
+      <li>500 GB Storage</li>
+      <li>2 Users Allowed</li>
+      <li>Send up to 3 GB</li>
+    </ul>
+    <button>Learn more</button>
   </>
 );
 
