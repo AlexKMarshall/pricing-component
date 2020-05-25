aa216160b3a81bbaed7d52080e57093e1471ad0c
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-25 18:41:56 +0100

    Animate buttons on hover

--- src/pages/index.js
+++ src/pages/index.js
@@ -145,7 +145,8 @@ const IndexPage = () => {
           className={`w-full p-4 uppercase rounded-md 
           text-${colors.buttonText.normal} hover:text-${colors.buttonText.hover} focus:text-${colors.buttonText.hover} 
           bg-${colors.button} hover:bg-none focus:bg-none
-          border-2 border-transparent hover:border-${colors.divider} focus:border-${colors.divider} focus:outline-none`}
+          border-2 border-transparent hover:border-${colors.divider} focus:border-${colors.divider} focus:outline-none
+          transition-colors duration-300 ease-in-out`}
           style={{ backgroundSize: "110%", backgroundPositionX: "-2px" }} //Style hack to deal with transparent borders on gradient with border-radius
         >
           Learn more
